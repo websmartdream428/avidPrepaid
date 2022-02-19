da8c23821c3dafb5a1295b835e78c23100b40b86
websmartdream <webdream1108@gmail.com>
2022-02-20 05:20:40 +0800

    feat: divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Container from "./Container";
 
+import logo from "assets/image/logo.png";
+
 const Divider = () => {
   return (
     <div className="landing-divider">
-      <Container></Container>
+      <Container>
+        <div className="flex justify-between items-center gap-5">
+          <img src={logo} alt="logo" className="logo-img w-[9.5em]" />
+          <div className="h-[2px] bg-white w-[65em] ml-[65px]" />
+        </div>
+      </Container>
     </div>
   );
 };
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,3 +1,4 @@
+import Divider from "components/Divider";
 import React from "react";
 import AboutPart from "./AboutPart";
 import BenefitPart from "./BenefitPart";
@@ -13,6 +14,7 @@ const LandingPage = () => {
       <AboutPart />
       <ServicePart />
       <BenefitPart />
+      <Divider />
       <FooterPart />
     </div>
   );
